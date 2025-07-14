b28f5566e838c7b57bf56887ef0112a945268ea8
Virendra <narveer019@gmail.com>
2025-07-14 13:01:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from '../components/Navigation'
+import HeroSection from '../components/HeroSection'
+
+function App() {
+
+  return (
+    <>
+     <Navigation/>
+     <HeroSection/>
+    </>
+  )
+}
+
+export default App
+  
